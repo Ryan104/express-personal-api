bce10105205fb52c6eca0593091c4d01a426fa67
Ryan <ryan.d.elliott@gmail.com>
2017-10-11 16:49:50 -0600

    Setup profile endpoint with hardcoded object

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // require express and other modules
-var express = require('express'),
-    app = express();
+const express = require('express');
+const app = express();
 
 // parse incoming urlencoded form data
 // and populate the req.body object
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -13,6 +13,13 @@ app.use(bodyParser.json());
  ************/
 
 // var db = require('./models');
+const personalData = {
+  name: 'Ryan Elliott',
+  github_link: 'https://github.com/Ryan104',
+  github_profile_image: 'https://avatars1.githubusercontent.com/u/18182439?v=4&s=400&u=cf2f8ded10522a85fcac07d852995acd5af83b41',
+  current_city: 'Denver',
+  hobbies: ['Coding', 'Coding', 'Coding', 'Coding', 'Hiking']
+};
 
 /**********
  * ROUTES *
@@ -36,13 +43,12 @@ app.get('/', function homepage(req, res) {
  */
 
 app.get('/api', function api_index(req, res) {
-  // TODO: Document all your api endpoints below
   res.json({
     message: "Welcome to my personal api! Here's what you need to know!",
     documentation_url: "https://github.com/Ryan104/express-personal-api/blob/master/README.md", // CHANGE ME
     base_url: "https://infinite-hollows-62292.herokuapp.com",
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints", active: false},
+      {method: "GET", path: "/api", description: "Describes all available endpoints", active: true},
       {method: "GET", path: "/api/profile", description: "Data about me", active: false}, 
       {method: "GET", path: "/api/14ers", description: "See all current mountains", active: false},
       {method: "POST", path: "/api/14ers", description: "Add a mountain", active: false},
@@ -52,7 +58,11 @@ app.get('/api', function api_index(req, res) {
       {method: "DELETE", path: "/api/14ers/:id", description: "Edit a mountain with the given ID", active: false},
 
     ]
-  })
+  });
+});
+
+app.get('/api/profile', (req,res) => {
+  res.json(personalData);
 });
 
 /**********
