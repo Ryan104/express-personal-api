f6962949792e520b29925e825d34f2b779fdf1ec
Ryan <ryan.d.elliott@gmail.com>
2017-10-11 20:11:32 -0600

    Create seed.js file

--- routes/mountain.js
+++ routes/mountain.js
@@ -4,38 +4,12 @@ const router = express.Router();
 // models
 const { Mountain } = require('../models');
 
-//fake data
-const mountains = [
-	{
-		name: 'Mt. Bierstadt',
-		elevation: 14060,
-		summitLocation: {
-			lat: '39.582638° N',
-			lon: '-105.668610° W'
-		},
-		routeName: 'West Slopes',
-		routeLength: 7.00,
-		routeClass: 2,
-		link: 'https://www.14ers.com/route.php?route=bier1&peak=Mt.+Bierstadt'
-	},
-	{
-		name: 'Quandry Peak',
-		elevation: 14265,
-		summitLocation: {
-			lat: '39.397236° N',
-			lon: '-106.106430° W'
-		},
-		routeName: 'East Ridge',
-		routeLength: 6.75,
-		routeClass: 2,
-		link: 'https://www.14ers.com/route.php?route=quan1&peak=Quandary+Peak'
-	}
-];
-
 //routes
 router.route('/14ers')
 	.get((req, res) => {
-		res.json(mountains);
+		Mountain.find({}, (err, mountains) => {
+			res.json(mountains);
+		});
 	})
 	.post((req, res) => {
 		console.log(req.body);
--- seed.js
+++ seed.js
@@ -1,9 +1,47 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+const db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+const mountains = [
+	{
+		name: 'Mt. Bierstadt',
+		elevation: 14060,
+		summitLocation: {
+			lat: '39.582638° N',
+			lon: '-105.668610° W'
+		},
+		routeName: 'West Slopes',
+		routeLength: 7.00,
+		routeClass: 2,
+		link: 'https://www.14ers.com/route.php?route=bier1&peak=Mt.+Bierstadt'
+	},
+	{
+		name: 'Quandry Peak',
+		elevation: 14265,
+		summitLocation: {
+			lat: '39.397236° N',
+			lon: '-106.106430° W'
+		},
+		routeName: 'East Ridge',
+		routeLength: 6.75,
+		routeClass: 2,
+		link: 'https://www.14ers.com/route.php?route=quan1&peak=Quandary+Peak'
+	}
+];
+
+// clear db
+db.Mountain.remove({}, (err) => {
+	if (err) console.log(err);
+	console.log('Seed.js: cleared Mountain collection');
+	db.Mountain.create(mountains, err => {
+		if (err) console.log(err);
+		console.log('Seed.js: created seed mountain data');
+		process.exit(); // we're all done! Exit the program.
+	});
+});
+
+
 
 // db.Campsite.create(new_campsite, function(err, campsite){
 //   if (err){
